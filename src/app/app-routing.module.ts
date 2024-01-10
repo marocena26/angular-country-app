ffedf1b6d5ccf64439f995994fb169d366bd4412
Elena Arocena <elena.arocena@innoway.es>
2024-01-10 12:04:55 +0100

    feat(app): add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPagesComponent } from './shared/pages/about-pages/about-pages.component';
+import { ContactPagesComponent } from './shared/pages/contact-pages/contact-pages.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent,
   },
   {
     path: 'about',
     component: AboutPagesComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactPagesComponent,
   },
   {
     path: '**',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
 @NgModule({
-  declarations: [AboutPagesComponent, HomePageComponent],
-  imports: [CommonModule],
-  exports: [AboutPagesComponent, HomePageComponent ],
+  declarations: [AboutPagesComponent, HomePageComponent, SidebarComponent, ContactPagesComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [AboutPagesComponent, HomePageComponent, SidebarComponent ],
 })
 export class SharedModule {}
