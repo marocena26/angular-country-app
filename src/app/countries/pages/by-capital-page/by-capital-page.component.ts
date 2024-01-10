68c018ab3e2e963850f8d9affbea0788e082f053
Elena Arocena <elena.arocena@innoway.es>
2024-01-10 16:16:02 +0100

    feat(app): add other tables for region and name country

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -15,10 +15,6 @@ export class ByCapitalPageComponent {
   constructor(private countryService: CountriesService){}
 
   searchByCapital (term : string) {
-
-    console.log('Desde ByCapitalPage');
-    console.log({term});
-
     this.countryService.searchCapital(term).subscribe( countries => {
       this.countries = countries;
     })
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-region-page',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./by-region-page.component.css']
 })
 export class ByRegionPageComponent {
+  public countries: Country[] = [];
+
+  constructor(private countryService: CountriesService){}
+
+  searchByRegion (term : string) {
+    this.countryService.searchRegion(term).subscribe( countries => {
+      this.countries = countries;
+    })
+
+  }
 
 }
