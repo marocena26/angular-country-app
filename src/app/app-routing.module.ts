fdf8e9c35b31175d14bafbf859ed894effa65cd4
Elena Arocena <elena.arocena@innoway.es>
2024-01-10 13:00:13 +0100

    feat(app): add news element for search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPagesComponent } from './shared/pages/about-pages/about-pages.component';
 import { ContactPagesComponent } from './shared/pages/contact-pages/contact-pages.component';
+import { CountriesModule } from './countries/countries.module';
 
 const routes: Routes = [
   {
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'contact',
     component: ContactPagesComponent,
+  },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule)
   },
   {
     path: '**',
